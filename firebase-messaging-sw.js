a73230b09b2c0f9eb563eea0f8ba76da862ed91a
sergeyaltcraft <sergey@sergey-eroshkin>
2021-04-22 18:44:09 +0300

    dfgfdgdhjjjf

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,27 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
-firebase.initializeApp({
-    messagingSenderId: '554490756447'
+importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAICFPo45m8tutTHTAlhxqtm8BZzLbpwr8",
+    authDomain: "sergeyaltcraft.firebaseapp.com",
+    databaseURL: "https://sergeyaltcraft-default-rtdb.firebaseio.com",
+    projectId: "sergeyaltcraft",
+    storageBucket: "sergeyaltcraft.appspot.com",
+    messagingSenderId: "554490756447",
+    appId: "1:554490756447:web:d39ac9647b7a62dea08aea",
+    measurementId: "G-64L0EQDKZG"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging=firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log(payload);
+    const notification=JSON.parse(payload);
+    const notificationOption={
+        body:notification.body,
+        icon:notification.icon
+    };
+    return self.registration.showNotification(payload.notification.title,notificationOption);
 });
-
-const messaging = firebase.messaging();
