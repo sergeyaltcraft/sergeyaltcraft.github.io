0d6818ba6464555e50af69cf4a34c2d84fb301ad
sergeyaltcraft <sergey@sergey-eroshkin>
2021-04-22 17:14:21 +0300

    dfsgsd

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -16,7 +16,7 @@ if ('Notification' in window) {
 
     // по клику, запрашиваем у пользователя разрешение на уведомления
     // и подписываем его
-    $('#subscribe').on('click', function () {
+    ('#subscribe').on('click', function () {
         subscribe();
     });
 }
